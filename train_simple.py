61a4aa09ccb6fe3e711cee9c79bf490eaaec46f4
HartAndrin-1024 <hartandrin1024@gmail.com>
2024-12-03 01:26:27 +0000

    Adding more files for my first pass

--- train_simple.py
+++ train_simple.py
@@ -46,67 +46,6 @@ MODEL_CONFIGS = [
             and torch.cuda.device_count() > 1
         ),
     ),
-    ModelConfig(
-        name="Qwen/Qwen-1_8B",
-        default_lr=1e-5,
-        eval_batch_size=2,
-        gradient_checkpointing=True,
-        model_parallel=(
-            torch.cuda.get_device_properties(0).total_memory < 35e9
-            and torch.cuda.device_count() > 1
-        ),
-        custom_kwargs={
-            "trust_remote_code": True,
-            "bf16": torch.cuda.is_bf16_supported(),
-            "fp32": not torch.cuda.is_bf16_supported(),
-            "revision": "5fde88dff770a7d036847211f5d9d9705f0caa69",
-        },
-    ),
-    ModelConfig(
-        name="Qwen/Qwen-7B",
-        default_lr=1e-5,
-        eval_batch_size=2,
-        gradient_checkpointing=True,
-        model_parallel=True,
-        # note: you will probably not be able to run this without many gpus
-        custom_kwargs={
-            "trust_remote_code": True,
-            "bf16": torch.cuda.is_bf16_supported(),
-            "fp32": not torch.cuda.is_bf16_supported(),
-            "revision": "d4efd21e866b9cb3466cb65b963933f5e98016d1",
-        },
-    ),
-    ModelConfig(
-        name="Qwen/Qwen-14B",
-        default_lr=1e-5,
-        eval_batch_size=2,
-        gradient_checkpointing=True,
-        model_parallel=True,
-        # note: you will probably not be able to run this bf16 support and without many gpus
-        custom_kwargs={
-            "trust_remote_code": True,
-            "bf16": torch.cuda.is_bf16_supported(),
-            "fp32": not torch.cuda.is_bf16_supported(),
-            "revision": "8be2854218fea9054331e217fd26a06f3fd02004",
-        },
-    ),
-    ModelConfig(
-        name="Qwen/Qwen-72B",
-        default_lr=1e-5,
-        eval_batch_size=1,
-        gradient_checkpointing=True,
-        model_parallel=True,
-        # note: you will probably not be able to run this without bf16 support and many gpus
-        custom_kwargs={
-            "trust_remote_code": True,
-            "bf16": torch.cuda.is_bf16_supported(),
-            "fp32": not torch.cuda.is_bf16_supported(),
-            "revision": "fec78c0e3b3b10dd9f0ce775c34a686a3255a7d1",
-        },
-        # This model is really big, save space by using adafactor.
-        # Note that even then it will take up ~60GB per GPU on an 8-GPU machine.
-        default_optimizer="adafactor",
-    ),
 ]
 MODELS_DICT: Dict[str, ModelConfig] = {
     model_config.name: model_config for model_config in MODEL_CONFIGS
@@ -144,7 +83,7 @@ def get_config_foldername(config: dict) -> str:
 def main(
     batch_size: int = 32,
     max_ctx: int = 1024,
-    ds_name: str = "sciq",
+    ds_name: str = "boolq",
     loss: str = "xent",
     n_docs: int = 20000,
     n_test_docs: int = 10000,
@@ -235,6 +174,16 @@ def main(
 
     # Split the training dataset in half
     train_dataset, test_ds = dataset["train"], dataset["test"]
+    # debug=True
+
+    # import ipdb
+    # ipdb.set_trace()
+
+    # if debug:
+    #     train_dataset=train_dataset[:100]
+    #     test_ds=test_ds[:100]
+
+    # ipdb.set_trace()
 
     if weak_labels_path is None:
         split_data = train_dataset.train_test_split(test_size=0.5, seed=seed)
